c0b6a4bd220edc483b2203607723959a36c4115a
J0N7E <henriksson.jonas@hotmail.com>
2023-08-08 11:00:06 +0200

    2023-08-08 11:00

--- VMSetupCAConfigureWebServer.ps1
+++ VMSetupCAConfigureWebServer.ps1
@@ -890,6 +890,20 @@ Begin
             }
         }
 
+        #  ██████╗███████╗██████╗ ████████╗███████╗██████╗ ██╗   ██╗
+        # ██╔════╝██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║   ██║
+        # ██║     █████╗  ██████╔╝   ██║   ███████╗██████╔╝██║   ██║
+        # ██║     ██╔══╝  ██╔══██╗   ██║   ╚════██║██╔══██╗╚██╗ ██╔╝
+        # ╚██████╗███████╗██║  ██║   ██║   ███████║██║  ██║ ╚████╔╝
+        #  ╚═════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝  ╚═══╝
+
+        # v2 template
+        # CSP
+        # IE: Truisted zone
+
+        # IE: Initialize and script ActiveX controls not marked as safe for scripting
+        # https://www.sysadmins.lv/retired-msft-blogs/alejacma/how-to-disable-this-web-site-is-attempting-to-perform-a-digital-certificate-operation-on-your-behalf-message.aspx
+
         #  ███╗   ██╗██████╗ ███████╗███████╗
         #  ████╗  ██║██╔══██╗██╔════╝██╔════╝
         #  ██╔██╗ ██║██║  ██║█████╗  ███████╗
